0aae79423bd3654153876e5f3b28e021a9139117
Arjun Rasodha <arjunrasodha10@gmail.com>
2022-03-17 21:12:18 -0400

    MongoDB connected

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const Person = require('./models/person.js');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 
 app.use(express.static('build'));
 app.use(cors());
@@ -11,36 +13,15 @@ app.use(express.json());
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(":method :url :status - :response-time ms - :res[content-length] :body"));
 
-let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
-
-const generateId = () => {
-  let maxId = persons.length > 0 ? Math.max(...persons.map(p => p.id)) : 0;
-  return maxId + 1;
-}
+// const generateId = () => {
+//   let maxId = persons.length > 0 ? Math.max(...persons.map(p => p.id)) : 0;
+//   return maxId + 1;
+// }
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons);
+  Person.find({}).then(persons => {
+    response.json(persons);
+  })
 });
 
 app.get('/info', (request, response) => {
@@ -50,35 +31,42 @@ app.get('/info', (request, response) => {
 });
 
 app.get('/api/persons/:id', (request, response) => {
-  let id = Number(request.params.id);
-  let person = persons.find(person => person.id === id);
-
-  if (person) {
+  Person.findById(request.params.id).then(person => {
     response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  })
+
+  // let id = Number(request.params.id);
+  // let person = persons.find(person => person.id === id);
+
+  // if (person) {
+  //   response.json(person);
+  // } else {
+  //   response.status(404).end();
+  // }
 })
 
 app.post('/api/persons', (request, response) => {
   let body = request.body;
 
   if (!body.name || !body.number) {
-    return response.status(404).json({ error: 'name and number must both be provided' });
-  } else if (persons.map(p => p.name.toLowerCase()).includes(body.name.toLowerCase())) {
-    return response.status(404).json({ error: 'name must be unique' });
-  } else if (persons.map(p => p.number).includes(body.number)) {
     return response.status(404).json({ error: 'number must be unique' });
   }
+  // if (!body.name || !body.number) {
+  //   return response.status(404).json({ error: 'name and number must both be provided' });
+  // } else if (persons.map(p => p.name.toLowerCase()).includes(body.name.toLowerCase())) {
+  //   return response.status(404).json({ error: 'name must be unique' });
+  // } else if (persons.map(p => p.number).includes(body.number)) {
+  //   return response.status(404).json({ error: 'number must be unique' });
+  // }
 
-  const person = {
-    id: generateId(),
+  const person = new Person({
     name: body.name,
     number: body.number
-  }
+  });
 
-  persons = persons.concat(person);
-  response.json(person);
+  person.save().then(savedPerson => {
+    response.json(savedPerson);
+  })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
